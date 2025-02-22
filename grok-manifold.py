26ba8de712342efd9bbbabf996a86a82597f512d
kha1n3vol3 <khunt@starficient.com>
2025-02-22 14:07:12 -0500

    Update grok-manifold.py
    add license to module.

--- grok-manifold.py
+++ grok-manifold.py
@@ -47,6 +47,27 @@ Usage:
 This content has passed all internal security controls and is fully approved for 
 both internal and external projects under the "Starficient ^ Above Efficient" initiative.
 © 2025 Starficient.
+MIT License
+
+Copyright (c) 2025 Starficient / kha1n3vol3
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 """
 
 import os
